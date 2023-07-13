f5b8ddbc6c2405680dde571105685dc511838bac
Hasegawa-Akito <h.akito1999@gmail.com>
2023-07-13 10:51:24 +0900

    storybook作成

---
+++ stories/Circle.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Circle from "./Circle";
+
+const meta: Meta<typeof Circle> = {
+  component: Circle,
+  title: "Circle",
+  //   tags: ['autodocs'],
+  //   argTypes: {
+  //     backgroundColor: {
+  //       control: 'color',
+  //     },
+  //   },
+};
+
+export default meta;
+
+export const BaseCircle = {};
